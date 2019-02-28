4295880a45f19d79d713eec0572fa288789c0071
Salvatore Marino <salv.marino@gmail.com>
2019-03-01 00:30:55 +0100

    Add Post Type for 'Product'

---
+++ wp-content/plugins/wp-products-plugin/include/posttypes.php
@@ -0,0 +1,79 @@
+<?php
+
+add_action( 'init', 'wp_product_plugin_create_product' );
+
+function wp_product_plugin_create_product() {
+    register_post_type(
+        'product',
+        array(
+            'labels' => array(
+                'name'               => __('Product', 'wp-product'),
+                'singular_name'      => __('Product', 'wp-product'),
+                'add_new'            => __('Add New', 'wp-product'),
+                'add_new_item'       => __('Add New product', 'wp-product'),
+                'edit_item'          => __('Edit product', 'wp-product'),
+                'new_item'           => __('New product', 'wp-product'),
+                'all_items'          => __('All product', 'wp-product'),
+                'view_item'          => __('View product', 'wp-product'),
+                'search_items'       => __('Search product', 'wp-product'),
+                'not_found'          => __('No product found', 'wp-product'),
+                'not_found_in_trash' => __('No product found in Trash', 'wp-product'),
+                'parent_item_colon'  => '',
+                'menu_name'          => __('Product', 'wp-product')
+            ),
+            'public'             => true,
+            'publicly_queryable' => true,
+            'show_ui'            => true,
+            'show_in_menu'       => true,
+            'query_var'          => true,
+            'has_archive'        => true,
+            'hierarchical'       => false,
+            'menu_position'      => 5,
+            'supports'           => array( 'title', 'thumbnail' )
+        )
+        );
+}
+
+function wp_product_plugin_add_box()
+{
+    add_meta_box(
+        'wp_product_plugin_box_id',           // Unique ID
+        'Stars',                              // Box title
+        'wp_product_plugin_box_html',         // Content callback, must be of type callable
+        'product',                            // Post type
+        'normal',                             // The context within the screen where the boxes should display.
+        'high'                                // The priority within the context where the boxes should show
+        );
+}
+add_action('add_meta_boxes', 'wp_product_plugin_add_box');
+
+function wp_product_plugin_box_html($post)
+{
+    /* Check if there is any meta value stored in the database in order to set the selected option using the function selected() */
+    $stars_field_value  = get_post_meta( $post->ID);
+    $selected_element = $stars_field_value['_wp_product_plugin_meta_key'][0];
+    
+    ?>
+    <label for="wp_product_plugin_field">Select the number of stars that will be used to sort the Products</label><br/><br/>
+    <select name="wp_product_plugin_field" id="wp_product_plugin_field" class="postbox">
+        <option value="" selected disabled hidden>Choose here</option>
+        <option value="1" <?php selected($selected_element, '1'); ?>>1</option>
+        <option value="2" <?php selected($selected_element, '2'); ?>>2</option>
+        <option value="3" <?php selected($selected_element, '3'); ?>>3</option>
+        <option value="4" <?php selected($selected_element, '4'); ?>>4</option>
+        <option value="5" <?php selected($selected_element, '5'); ?>>5</option>
+    </select>
+    <?php
+}
+
+function wp_product_plugin_save_postdata($post_id)
+{
+    if (array_key_exists('wp_product_plugin_field', $_POST)) {
+        update_post_meta(
+            $post_id,
+            '_wp_product_plugin_meta_key',
+            $_POST['wp_product_plugin_field']
+            );
+    }
+}
+add_action('save_post', 'wp_product_plugin_save_postdata');
---
+++ wp-content/plugins/wp-products-plugin/wp-products-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WP Products Plugin
+ * Description: This plugin manages the creation of a new custom post type "Product", the widget "Product List", the taxonomy "Target Groups" and the setting for "Default Target Group"
+ * Author: Salvatore Marino
+ * Version: 1.0.0
+ */
+
+include_once( dirname( __FILE__ ) . '/include/posttypes.php' );
