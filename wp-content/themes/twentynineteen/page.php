1d0eb4bd4c060044db58a7e25aa23a8af9e3d23f
Salvatore Marino <salv.marino@gmail.com>
2024-12-05 12:53:17 +0100

    Update page.php

--- wp-content/themes/twentynineteen/page.php
+++ wp-content/themes/twentynineteen/page.php
@@ -1,38 +1,20 @@
 <?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package WordPress
- * @subpackage Twenty_Nineteen
- * @since 1.0.0
- */
-
-get_header();
+/*
+Template Name: Custom Page Template
+*/
 ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-			<?php
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				get_template_part( 'template-parts/content/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php
-get_footer();
+<!DOCTYPE html>
+<html lang='en'>
+ <head>
+  <title>Basic HTML5 document</title>
+  <meta charset='utf-8'>
+  <style>
+  </style>
+  <script>
+  </script>
+ </head>
+ <body>
+    <p>THIS IS A TEST</p>
+ </body>
+</html>
